4f42a353980c3e08442cdb3a9c40ba35bde6a397
gdncomm-james-wongso <james.wongso@gdn-commerce.com>
2025-12-31 22:37:48 +0700

    Run php-fpm without su wrapper

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -27,8 +27,4 @@ if [ -z "$APP_KEY" ]; then
   echo "WARNING: APP_KEY is not set. Generate one before going to production."
 fi
 
-if [ "$(id -u)" -eq 0 ]; then
-  exec su -s /bin/sh www-data -c "$*"
-else
-  exec "$@"
-fi
+exec "$@"
